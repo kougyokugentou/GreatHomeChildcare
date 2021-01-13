157db5042280168c5decb184970abd1ce1f1fc70
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-01-12 22:22:16 -0800

    add authorized_guardian as the bridge table for many-to-many children<->guardians

---
+++ Models/Authorized_Guardian.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatHomeChildcare.Models
+{
+    /* The authorized_guardian class and table acts
+     * as a bridge table in the database to provide
+     * a many-to-many mapping of children to guardians.
+     * Therefore, one child can have many guardians;
+     * and a single guardian can have many children.
+     * eg:
+     * id   child_id    guardian_id
+     * 1    1           1
+     * 2    1           2
+     * 3    1           3
+     * 4    2           1
+     */
+    class Authorized_Guardian
+    {
+        public int id { get; set; }
+        public int child_id { get; set; }
+        public int guardian_id { get; set; }
+    }
+}
