bf174c3003a111f2bec75b6f82db1e03ade9ce8d
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-01-13 20:14:00 -0800

    somehow i lost some numbers, fix that.

--- frmPinEntry.Designer.cs
+++ frmPinEntry.Designer.cs
@@ -42,6 +42,9 @@ namespace GreatHomeChildcare
             this.lblWelcome = new System.Windows.Forms.Label();
             this.tbPinNumber = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,31 +54,34 @@ namespace GreatHomeChildcare
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.btn0, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btnBack, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btn6, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btnCE, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btn5, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btn8, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnCE, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn0, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnBack, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btn9, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(74, 88);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(71, 85);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33592F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.3326F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33148F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(340, 324);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // btn0
             // 
             this.btn0.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn0.Location = new System.Drawing.Point(116, 218);
+            this.btn0.Location = new System.Drawing.Point(116, 246);
             this.btn0.Name = "btn0";
-            this.btn0.Size = new System.Drawing.Size(107, 103);
+            this.btn0.Size = new System.Drawing.Size(107, 75);
             this.btn0.TabIndex = 10;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
@@ -84,9 +90,9 @@ namespace GreatHomeChildcare
             // btnBack
             // 
             this.btnBack.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnBack.Location = new System.Drawing.Point(229, 218);
+            this.btnBack.Location = new System.Drawing.Point(229, 246);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(108, 103);
+            this.btnBack.Size = new System.Drawing.Size(108, 75);
             this.btnBack.TabIndex = 9;
             this.btnBack.Text = "<<";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -94,10 +100,9 @@ namespace GreatHomeChildcare
             // 
             // btn6
             // 
-            this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn6.Location = new System.Drawing.Point(229, 111);
+            this.btn6.Location = new System.Drawing.Point(229, 84);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(108, 101);
+            this.btn6.Size = new System.Drawing.Size(108, 75);
             this.btn6.TabIndex = 8;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
@@ -106,9 +111,9 @@ namespace GreatHomeChildcare
             // btnCE
             // 
             this.btnCE.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnCE.Location = new System.Drawing.Point(3, 218);
+            this.btnCE.Location = new System.Drawing.Point(3, 246);
             this.btnCE.Name = "btnCE";
-            this.btnCE.Size = new System.Drawing.Size(107, 103);
+            this.btnCE.Size = new System.Drawing.Size(107, 75);
             this.btnCE.TabIndex = 7;
             this.btnCE.Text = "CE";
             this.btnCE.UseVisualStyleBackColor = true;
@@ -116,10 +121,9 @@ namespace GreatHomeChildcare
             // 
             // btn5
             // 
-            this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn5.Location = new System.Drawing.Point(116, 111);
+            this.btn5.Location = new System.Drawing.Point(116, 84);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(107, 101);
+            this.btn5.Size = new System.Drawing.Size(107, 75);
             this.btn5.TabIndex = 6;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
@@ -127,10 +131,9 @@ namespace GreatHomeChildcare
             // 
             // btn8
             // 
-            this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn8.Location = new System.Drawing.Point(116, 3);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(107, 102);
+            this.btn8.Size = new System.Drawing.Size(107, 75);
             this.btn8.TabIndex = 5;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
@@ -138,10 +141,9 @@ namespace GreatHomeChildcare
             // 
             // btn7
             // 
-            this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn7.Location = new System.Drawing.Point(3, 3);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(107, 102);
+            this.btn7.Size = new System.Drawing.Size(107, 75);
             this.btn7.TabIndex = 4;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
@@ -149,10 +151,9 @@ namespace GreatHomeChildcare
             // 
             // btn4
             // 
-            this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn4.Location = new System.Drawing.Point(3, 111);
+            this.btn4.Location = new System.Drawing.Point(3, 84);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(107, 101);
+            this.btn4.Size = new System.Drawing.Size(107, 75);
             this.btn4.TabIndex = 3;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
@@ -160,10 +161,9 @@ namespace GreatHomeChildcare
             // 
             // btn9
             // 
-            this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn9.Location = new System.Drawing.Point(229, 3);
             this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(108, 102);
+            this.btn9.Size = new System.Drawing.Size(108, 75);
             this.btn9.TabIndex = 2;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
@@ -199,6 +199,39 @@ namespace GreatHomeChildcare
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.Location = new System.Drawing.Point(3, 165);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(107, 75);
+            this.btn1.TabIndex = 11;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnNumButton_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn2.Location = new System.Drawing.Point(116, 165);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(107, 75);
+            this.btn2.TabIndex = 12;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btnNumButton_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn3.Location = new System.Drawing.Point(229, 165);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(108, 75);
+            this.btn3.TabIndex = 13;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btnNumButton_Click);
+            // 
             // frmPinEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -232,6 +265,9 @@ namespace GreatHomeChildcare
         private System.Windows.Forms.Label lblWelcome;
         private System.Windows.Forms.TextBox tbPinNumber;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
     }
 }
 
