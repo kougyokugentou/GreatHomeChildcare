450a002ef90616a0db8bafeb308ff7578b35b104
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-01-17 17:57:00 -0800

    Shell: main admin form

---
+++ frmAdminForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreatHomeChildcare
+{
+    public partial class frmAdminForm : Form
+    {
+        public frmAdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAdminForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
