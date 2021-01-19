c0375914516f72d08efe53f8a282ed3bd729574b
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-01-18 20:21:36 -0800

    add documentation

---
+++ docs/Create_Table_Schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "Guardians" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"FirstName"	TEXT NOT NULL,
+	"LastName"	TEXT NOT NULL,
+	"PhoneNumber"	INTEGER NOT NULL,
+	"EmailAddress"	TEXT NOT NULL,
+	"PinNumber" INTEGER NOT NULL UNIQUE,
+	"isAdmin" INTEGER NOT NULL
+);
+
+CREATE TABLE "Children" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"FirstName"	TEXT NOT NULL,
+	"LastName"	TEXT COLLATE nocase NOT NULL,
+	"DOB"	TEXT NOT NULL,
+	"address"	TEXT NOT NULL,
+	"race"	TEXT NOT NULL,
+	"gender"	TEXT NOT NULL,
+	"photo"	BLOB
+);
+
+CREATE TABLE "Attendence" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"child_id"	INTEGER NOT NULL,
+	"guardian_id"	INTEGER NOT NULL,
+	"in_out"	TEXT NOT NULL,
+	"timestamp" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY("child_id") REFERENCES "Children"("id"),
+	FOREIGN KEY("guardian_id") REFERENCES "Guardians"("id")
+);
+
+CREATE TABLE "Authorized_Guardians" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"child_id" INTEGER NOT NULL,
+	"guardian_id" INTEGER NOT NULL,
+	FOREIGN KEY("child_id") REFERENCES "Children"("id"),
+	FOREIGN KEY("guardian_id") REFERENCES "Guardians"("id")	
+);
---
+++ docs/GenerateMockData.sql
@@ -0,0 +1,14 @@
+INSERT INTO Children(FirstName,LastName,DOB,address,race,gender) VALUES ('Joe','Smith','1/13/2021 4:56:33 PM','123 Fake Street','White','Male');
+INSERT INTO Children(FirstName,LastName,DOB,address,race,gender) VALUES ('Mary','Smith','1/13/2021 4:56:33 PM','123 Fake Street','White','Female');
+INSERT INTO Children(FirstName,LastName,DOB,address,race,gender) VALUES ('Katie','Admin','1/13/2021 4:56:33 PM','123 Fake Street','White','Male');
+
+INSERT INTO Guardians(FirstName,LastName,PhoneNumber,EmailAddress,PinNumber,isAdmin) VALUES ('Main','Admin',1000000001,'main@admin.com','9999',1);
+INSERT INTO Guardians(FirstName,LastName,PhoneNumber,EmailAddress,PinNumber) VALUES ('Parent','Smith',1000000001,'parent@smith.com','1234');
+
+INSERT INTO Authorized_Guardians(child_id, guardian_id) VALUES (1,2);
+INSERT INTO Authorized_Guardians(child_id, guardian_id) VALUES (2,2);
+INSERT INTO Authorized_Guardians(child_id, guardian_id) VALUES (3,1);
+
+INSERT INTO Attendence(child_id, guardian_id,in_out) VALUES (1,2,"in");
+INSERT INTO Attendence(child_id, guardian_id,in_out) VALUES (2,2,"out");
+INSERT INTO Attendence(child_id, guardian_id,in_out) VALUES (3,1,"out");
