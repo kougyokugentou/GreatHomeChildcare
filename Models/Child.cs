5a8a98b7c15422e20ffc3dc753909f515e96564d
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-02-03 09:08:47 -0800

    Update: DisplayName property for get/set for the report site.

--- Models/Child.cs
+++ Models/Child.cs
@@ -2,6 +2,8 @@
 {
     public class Child
     {
+        private string displayName;
+
         public int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -11,12 +13,28 @@
         public string gender { get; set; }
         public byte[] photo { get; set; }
 
-        //readonly property to populate a single child's full name.
-        // this is a "Get" only property
+        /* Ppopulate a single child's full name.
+         * "get" will return Lastname, Fistname if the lastname is not null.
+         * otherwise it will return the variable displayName.
+         * This is so we can insert "Everyone" into the list
+         * of children for the reports form.
+         */
         public string DisplayName
         {
-            get =>
-                $"{LastName}, {FirstName}";
+            get
+            {
+                //This is so we can have "everyone" in the list of
+                //children in the reports form.
+                if (LastName != null)
+                    { return $"{LastName}, {FirstName}"; }
+                else
+                    return displayName;
+            }
+                
+            set
+            {
+                displayName = value;
+            }
         }
     }
 }
