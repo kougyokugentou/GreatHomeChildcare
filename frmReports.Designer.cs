6a0fef9309264403fe43b491439e983ecda58fce
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-02-03 09:10:30 -0800

    Add filter combobox for child selection.

--- frmReports.Designer.cs
+++ frmReports.Designer.cs
@@ -29,19 +29,47 @@ namespace GreatHomeChildcare
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbChildPicker = new System.Windows.Forms.ComboBox();
+            this.lblFilter = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cbChildPicker
+            // 
+            this.cbChildPicker.DisplayMember = "DisplayName";
+            this.cbChildPicker.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbChildPicker.FormattingEnabled = true;
+            this.cbChildPicker.Location = new System.Drawing.Point(13, 57);
+            this.cbChildPicker.Name = "cbChildPicker";
+            this.cbChildPicker.Size = new System.Drawing.Size(121, 24);
+            this.cbChildPicker.TabIndex = 0;
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(13, 22);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(39, 17);
+            this.lblFilter.TabIndex = 1;
+            this.lblFilter.Text = "Filter";
             // 
             // frmReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.cbChildPicker);
             this.Name = "frmReports";
             this.Text = "Reports : Great Home Childcare";
+            this.Load += new System.EventHandler(this.frmReports_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbChildPicker;
+        private System.Windows.Forms.Label lblFilter;
     }
 }
--- frmReports.cs
+++ frmReports.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GreatHomeChildcare.Models;
 
 namespace GreatHomeChildcare
 {
     public partial class frmReports : Form
     {
+        //globals for cheap access.
+        SqliteDataAccess SqliteDataAccess = new SqliteDataAccess();
+
         public frmReports()
         {
             InitializeComponent();
+        }
+
+        private void frmReports_Load(object sender, EventArgs e)
+        {
+            LoadFilterComboBox();
+        }
+
+        private void LoadFilterComboBox()
+        {
+            List<Child> children = new List<Child>();
+            Child everyoneSelection = new Child();
+
+            /* cheap hack since you can't add an item to
+             * a combobox that has existing databinding.
+             */
+            everyoneSelection.id = 0;
+            everyoneSelection.DisplayName = "Everyone";
+
+            children = SqliteDataAccess.GetAllChildren();
+            children.Insert(0, everyoneSelection);
+
+            cbChildPicker.DataSource = children;
         }
     }
 }
