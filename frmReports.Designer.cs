30b4d6a657124e30dba82ad69f467655337826d5
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-02-05 23:00:37 -0800

    Add some constants. Clear the columns from dgv on formload, we don't need those. Refresh the report datagrid view on date change. Implement the logic to show datagrid view for all children. Break out the ReportScreen data into a seperate function. Implement save to CSV.

--- frmReports.Designer.cs
+++ frmReports.Designer.cs
@@ -31,6 +31,8 @@ namespace GreatHomeChildcare
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReports));
             this.cbChildPicker = new System.Windows.Forms.ComboBox();
             this.lblFilter = new System.Windows.Forms.Label();
             this.dtpFrom = new System.Windows.Forms.DateTimePicker();
@@ -38,7 +40,16 @@ namespace GreatHomeChildcare
             this.lblFrom = new System.Windows.Forms.Label();
             this.lblTo = new System.Windows.Forms.Label();
             this.dgvReports = new System.Windows.Forms.DataGridView();
-            this.ChildDisplayName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChildName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.in_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GInDisplayName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.out_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GOutDisplayname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSavetoSV = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.saveFileDialogReport = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReports)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,6 +90,7 @@ namespace GreatHomeChildcare
             this.dtpTo.Name = "dtpTo";
             this.dtpTo.Size = new System.Drawing.Size(121, 22);
             this.dtpTo.TabIndex = 3;
+            this.dtpTo.CloseUp += new System.EventHandler(this.dtpTo_CloseUp);
             // 
             // lblFrom
             // 
@@ -104,28 +116,114 @@ namespace GreatHomeChildcare
             this.dgvReports.AllowUserToDeleteRows = false;
             this.dgvReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvReports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ChildDisplayName});
+            this.ChildName,
+            this.in_time,
+            this.GInDisplayName,
+            this.out_time,
+            this.GOutDisplayname,
+            this.Duration});
             this.dgvReports.Location = new System.Drawing.Point(204, 39);
             this.dgvReports.Name = "dgvReports";
             this.dgvReports.ReadOnly = true;
             this.dgvReports.RowHeadersWidth = 51;
             this.dgvReports.RowTemplate.Height = 24;
-            this.dgvReports.Size = new System.Drawing.Size(565, 367);
+            this.dgvReports.Size = new System.Drawing.Size(846, 367);
             this.dgvReports.TabIndex = 6;
             // 
-            // ChildDisplayName
+            // ChildName
             // 
-            this.ChildDisplayName.HeaderText = "Child";
-            this.ChildDisplayName.MinimumWidth = 6;
-            this.ChildDisplayName.Name = "ChildDisplayName";
-            this.ChildDisplayName.ReadOnly = true;
-            this.ChildDisplayName.Width = 125;
+            this.ChildName.HeaderText = "ChildName";
+            this.ChildName.MinimumWidth = 6;
+            this.ChildName.Name = "ChildName";
+            this.ChildName.ReadOnly = true;
+            this.ChildName.Width = 125;
+            // 
+            // in_time
+            // 
+            this.in_time.HeaderText = "in_time";
+            this.in_time.MinimumWidth = 6;
+            this.in_time.Name = "in_time";
+            this.in_time.ReadOnly = true;
+            this.in_time.Width = 125;
+            // 
+            // GInDisplayName
+            // 
+            this.GInDisplayName.HeaderText = "Guardian In";
+            this.GInDisplayName.MinimumWidth = 6;
+            this.GInDisplayName.Name = "GInDisplayName";
+            this.GInDisplayName.ReadOnly = true;
+            this.GInDisplayName.Width = 125;
+            // 
+            // out_time
+            // 
+            this.out_time.HeaderText = "out_time";
+            this.out_time.MinimumWidth = 6;
+            this.out_time.Name = "out_time";
+            this.out_time.ReadOnly = true;
+            this.out_time.Width = 125;
+            // 
+            // GOutDisplayname
+            // 
+            this.GOutDisplayname.HeaderText = "Guardian Out";
+            this.GOutDisplayname.MinimumWidth = 6;
+            this.GOutDisplayname.Name = "GOutDisplayname";
+            this.GOutDisplayname.ReadOnly = true;
+            this.GOutDisplayname.Width = 125;
+            // 
+            // Duration
+            // 
+            this.Duration.HeaderText = "Duration";
+            this.Duration.MinimumWidth = 6;
+            this.Duration.Name = "Duration";
+            this.Duration.ReadOnly = true;
+            this.Duration.Width = 125;
+            // 
+            // btnSavetoSV
+            // 
+            this.btnSavetoSV.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSavetoSV.ImageIndex = 0;
+            this.btnSavetoSV.ImageList = this.imageList1;
+            this.btnSavetoSV.Location = new System.Drawing.Point(13, 364);
+            this.btnSavetoSV.Name = "btnSavetoSV";
+            this.btnSavetoSV.Size = new System.Drawing.Size(148, 42);
+            this.btnSavetoSV.TabIndex = 7;
+            this.btnSavetoSV.Text = "Save as CSV";
+            this.btnSavetoSV.UseVisualStyleBackColor = true;
+            this.btnSavetoSV.Click += new System.EventHandler(this.btnSavetoCSV_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "ResultToCSV_16x.png");
+            this.imageList1.Images.SetKeyName(1, "Print_16x.png");
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPrint.ImageIndex = 1;
+            this.btnPrint.ImageList = this.imageList1;
+            this.btnPrint.Location = new System.Drawing.Point(12, 427);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(149, 34);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // saveFileDialogReport
+            // 
+            this.saveFileDialogReport.DefaultExt = "CSV";
+            this.saveFileDialogReport.Filter = "CSV Files|*.csv";
+            this.saveFileDialogReport.Title = "Save Report";
             // 
             // frmReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1062, 469);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnSavetoSV);
             this.Controls.Add(this.dgvReports);
             this.Controls.Add(this.lblTo);
             this.Controls.Add(this.lblFrom);
@@ -133,6 +231,7 @@ namespace GreatHomeChildcare
             this.Controls.Add(this.dtpFrom);
             this.Controls.Add(this.lblFilter);
             this.Controls.Add(this.cbChildPicker);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmReports";
             this.Text = "Reports : Great Home Childcare";
             this.Load += new System.EventHandler(this.frmReports_Load);
@@ -151,6 +250,15 @@ namespace GreatHomeChildcare
         private System.Windows.Forms.Label lblFrom;
         private System.Windows.Forms.Label lblTo;
         private System.Windows.Forms.DataGridView dgvReports;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ChildDisplayName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChildName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn in_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GInDisplayName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn out_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GOutDisplayname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
+        private System.Windows.Forms.Button btnSavetoSV;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogReport;
     }
 }
--- frmReports.cs
+++ frmReports.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CsvHelper;
 using GreatHomeChildcare.Models;
 
 //REF:
@@ -19,6 +21,8 @@ namespace GreatHomeChildcare
         //globals for cheap access.
         SqliteDataAccess SqliteDataAccess = new SqliteDataAccess();
         List<Child> AllChildren = new List<Child>();
+        const string UNIXEPOCH = "1970-01-01";
+        const string DATE_TO_STRING = "yyyy-MM-dd";
 
         public frmReports()
         {
@@ -27,6 +31,12 @@ namespace GreatHomeChildcare
 
         private void frmReports_Load(object sender, EventArgs e)
         {
+            /* The columns are there for us in the designer view.
+             * Realistically we won't need them as the dgv is
+             * programatically bound to a data source.
+             */
+            dgvReports.Columns.Clear();
+
             //cannot select from a future date.
             this.dtpFrom.MaxDate = DateTime.Now;
 
@@ -78,33 +88,106 @@ namespace GreatHomeChildcare
         {
             dtpFrom.ValueChanged += dtpFrom_ValueChanged;
             dtpFrom_ValueChanged(sender, e);
+            RefreshReportDgv();
+        }
+
+        private void dtpTo_CloseUp(object sender, EventArgs e)
+        {
+            RefreshReportDgv();
         }
 
         //Refresh the datagrid view.
         private void RefreshReportDgv()
         {
+            dgvReports.DataSource = null;
+
             dgvReports.Rows.Clear();
             
             //First do the everyone case.
             if(cbChildPicker.Text == "Everyone")
             {
-                foreach(DateTime day in EachDay(dtpFrom.Value,dtpTo.Value))
-                {
-
-                }
-            }
-            else //load an individual child
-            {
-                Child child = (Child)cbChildPicker.SelectedItem;
-                AttendenceSingleInOutData in_data = new AttendenceSingleInOutData();
-                AttendenceSingleInOutData out_data = new AttendenceSingleInOutData();
+                List<ReportScreen> lstRs = new List<ReportScreen>();
 
                 foreach (DateTime day in EachDay(dtpFrom.Value, dtpTo.Value))
                 {
-                    in_data = SqliteDataAccess.GetAttendenceByStatusForChildByDay(child, "in", day.ToString("yyyy-MM-dd"));
-                    out_data = SqliteDataAccess.GetAttendenceByStatusForChildByDay(child, "out",day.ToString("yyyy-MM-dd"));
+                    //Double loop here is the only way I can think about it.
+                    foreach (Child child in AllChildren)
+                    {
+                        //Repeat of the code of an individual child.
+                        ReportScreen rs = new ReportScreen();
+                        rs = ReportChildDay(child, day);
+
+                        if (rs != null)
+                        {
+                            //push older events to the bottom.
+                            lstRs.Insert(0, rs);
+                        }
+                    }//foreach child
+                }//foreach day
+
+                //Load the datagrid.
+                dgvReports.DataSource = lstRs;
+            }// end everyone
+            else //load an individual child
+            {
+                Child child = (Child)cbChildPicker.SelectedItem;
+                List<ReportScreen> lstRs = new List<ReportScreen>();
+
+                foreach (DateTime day in EachDay(dtpFrom.Value, dtpTo.Value))
+                {
+                    ReportScreen rs = new ReportScreen();
+                    rs = ReportChildDay(child, day);
+
+                    if(rs != null)
+                    {
+                        //push older events to the bottom.
+                        lstRs.Insert(0, rs);
+                    }
                 }
+
+                //Load the datagrid.
+                dgvReports.DataSource = lstRs;
+            } //end else individual child
+        }
+
+        /* Gets sign/in out data for a single child given a single day.
+         * INPUT: child, day
+         * OUTPUT: reportscreen datatype class.
+         */
+        private ReportScreen ReportChildDay(Child child, DateTime day)
+        {
+            ReportScreen rs = new ReportScreen();
+            AttendenceSingleInOutData in_data = new AttendenceSingleInOutData();
+            AttendenceSingleInOutData out_data = new AttendenceSingleInOutData();
+
+            in_data = SqliteDataAccess.GetAttendenceByStatusForChildByDay(child, "in", day.ToString(DATE_TO_STRING));
+            out_data = SqliteDataAccess.GetAttendenceByStatusForChildByDay(child, "out", day.ToString(DATE_TO_STRING));
+
+            if (in_data == null)
+                return null;
+
+            rs.ChildName = $"{in_data.ChildLastName}, {in_data.ChildFirstName}";
+            rs.in_time = DateTime.Parse(in_data.timestamp);
+            rs.GuardianInName = $"{in_data.GuardianLastName}, {in_data.GuardianLastName}";
+
+            if (out_data != null) //Student's been checked out, display that data.
+            {
+                rs.out_time = DateTime.Parse(out_data.timestamp);
+                rs.GuardianOutName = $"{out_data.GuardianLastName}, {out_data.GuardianFirstName}";
             }
+            else //Student has not been checked out yet.
+            {
+                rs.out_time = DateTime.Parse(UNIXEPOCH);
+                rs.GuardianOutName = String.Empty;
+            }
+
+            if (rs.out_time != DateTime.Parse(UNIXEPOCH)) //check for null out_time.
+            {
+                TimeSpan duration = rs.out_time.Subtract(rs.in_time);
+                rs.Duration = duration.ToString();
+            }
+
+            return rs;
         }
 
         //https://stackoverflow.com/questions/1847580/how-do-i-loop-through-a-date-range
@@ -112,6 +195,79 @@ namespace GreatHomeChildcare
         {
             for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
                 yield return day;
+        }
+
+        private void btnSavetoCSV_Click(object sender, EventArgs e)
+        {
+            //If nothing's shown on screen, there's nothing to save.
+            if(dgvReports.Rows.Count < 1)
+            {
+                MessageBox.Show("There are no reports for that time frame. Choose another time frame and try again.", "Great Home Childcare", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return;
+            }
+
+            //Build the default filename
+            saveFileDialogReport.FileName = DateTime.Now.ToString("yy.MM.dd") + "-report.csv";
+
+            if (saveFileDialogReport.ShowDialog() == DialogResult.OK
+            && saveFileDialogReport.FileName != "")
+            {
+                string filename = saveFileDialogReport.FileName;
+                List<ReportData> lstReport = new List<ReportData>();
+                lstReport = GenerateReport();
+
+                //Now with that monkey business out of the way
+                //let's get on with actually saving the file.
+                TextWriter textWriter = new StreamWriter(filename);
+                CsvWriter csvWriter = new CsvWriter(textWriter);
+                csvWriter.WriteRecords(lstReport);
+
+                textWriter.Close();
+                MessageBox.Show("Report Saved", "Great Home Childcare", MessageBoxButtons.OK, MessageBoxIcon.None);
+            }
+        }
+
+        private List<ReportData> GenerateReport()
+        {
+            List<ReportData> lstReport = new List<ReportData>();
+
+            //First do the everyone case.
+            if (cbChildPicker.Text == "Everyone")
+            {
+                /* This is basically the same code as ReportChildDay,
+                 * but the query we are executing is different along with the datatype.
+                 */
+                foreach (DateTime day in EachDay(dtpFrom.Value, dtpTo.Value))
+                {
+                    List<ReportData> dayRd = new List<ReportData>();
+                    dayRd = SqliteDataAccess.GetReportData(day.ToString(DATE_TO_STRING), null);
+                    lstReport.AddRange(dayRd);
+                }
+            }
+            else //an individual child was selected.
+            {
+                Child child = (Child)cbChildPicker.SelectedItem;
+
+                foreach (DateTime day in EachDay(dtpFrom.Value, dtpTo.Value))
+                {
+                    List<ReportData> dayRd = new List<ReportData>();
+                    dayRd = SqliteDataAccess.GetReportData(day.ToString(DATE_TO_STRING), child);
+                    lstReport.AddRange(dayRd);
+                }
+            }
+            return lstReport;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            //If nothing's shown on screen, there's nothing to print.
+            if (dgvReports.Rows.Count < 1)
+            {
+                MessageBox.Show("There are no reports for that time frame. Choose another time frame and try again.", "Great Home Childcare", MessageBoxButtons.OK, MessageBoxIcon.None);
+                return;
+            }
+
+            //TODO: figure out a way to actually implement printing. It's one of the toolbox things.
         }
     }
 }
