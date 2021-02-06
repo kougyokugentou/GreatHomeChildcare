20da3f24e94d0866e6411d7fa758717ee12a6558
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2021-02-05 23:01:12 -0800

    Reports class

--- Models/Reports.cs
+++ Models/Reports.cs
@@ -6,9 +6,23 @@ using System.Threading.Tasks;
 
 namespace GreatHomeChildcare.Models
 {
-    class Reports
+    class ReportScreen
     {
         public string ChildName { get; set; }
+        public DateTime in_time { get; set; }
+        public string GuardianInName { get; set; }
+        public DateTime out_time { get; set; }
+        public string GuardianOutName { get; set; }
+        public string Duration { get; set; }
+    }
 
+    class ReportData
+    {
+        public string ChildFirstName { get; set; }
+        public string ChildLastName { get; set; }
+        public string in_out { get; set; }
+        public string GuardianFirstName { get; set; }
+        public string GuardianLastName { get; set; }
+        public string timestamp { get; set; }
     }
 }
